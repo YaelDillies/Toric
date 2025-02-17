f1669066433e5215ce9d5d8c3316812e49476e6f
Yaël Dillies <yael.dillies@gmail.com>
2025-02-17 16:13:06 +0000

    Definition of a variety

---
+++ Toric/Variety.lean
@@ -0,0 +1,39 @@
+/-
+Copyright (c) 2025 Yaël Dillies, Patrick Luo. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yaël Dillies, Patrick Luo
+-/
+import Mathlib.AlgebraicGeometry.Morphisms.Proper
+
+/-!
+# Varieties
+
+This files defines algebraic geometric varieties over a ring `R` as separated, integral, finite type
+morphisms from a scheme to `Spec R`.
+-/
+
+open CategoryTheory
+
+namespace AlgebraicGeometry
+variable {R : CommRingCat} {X Y Z : Scheme}
+
+/-- A morphism of schemes is a variety hom if it is separated, integral and of finite type. -/
+class IsVarietyHom ⦃X Y : Scheme⦄ (f : X ⟶ Y) : Prop where
+  [isSeparated : IsSeparated f]
+  [isIntegralHom : IsIntegralHom f]
+  -- TODO: Should be finite type, not merely locally of finite type
+  [locallyOfFiniteType : LocallyOfFiniteType f]
+
+namespace IsVarietyHom
+
+attribute [instance] isSeparated isIntegralHom locallyOfFiniteType
+
+@[simp] protected lemma id : IsVarietyHom (𝟙 X) where
+
+end IsVarietyHom
+
+variable (R) in
+/-- A variety over a ring `R` is a scheme `X` along with a separated, integral, finite type morphism `f : X ⟶ Spec R`. -/
+abbrev Variety := FullSubcategory fun X : Over (Spec R) ↦ IsVarietyHom X.hom
+
+end AlgebraicGeometry
