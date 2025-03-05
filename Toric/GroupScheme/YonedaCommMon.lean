6377199d6c98849d8d9a312b7d93097aa0052d66
Kiolt <38689041+Kiolt@users.noreply.github.com>
2025-03-05 22:40:03 +0000

    erw fix

--- Toric/GroupScheme/YonedaCommMon.lean
+++ Toric/GroupScheme/YonedaCommMon.lean
@@ -23,14 +23,13 @@ def IsCommMon.ofRepresentableBy (F : Cᵒᵖ ⥤ CommMonCat.{w})
     IsCommMon X := by
   letI := Mon_ClassOfRepresentableBy X (F ⋙ (forget₂ CommMonCat MonCat)) α
   refine ⟨?_⟩
-  change _ ≫ α.homEquiv.symm _ = α.homEquiv.symm _
+  change (β_ X X).hom ≫ α.homEquiv.symm (α.homEquiv (fst X X) * α.homEquiv (snd X X))
+      = α.homEquiv.symm (α.homEquiv (fst X X) * α.homEquiv (snd X X))
   apply α.homEquiv.injective
   simp only [α.homEquiv_comp, Equiv.apply_symm_apply]
   simp only [Functor.comp_map, ConcreteCategory.forget_map_eq_coe, map_mul]
-  simp only [← ConcreteCategory.forget_map_eq_coe, ← Functor.comp_map]
-  erw [← α.homEquiv_comp, ← α.homEquiv_comp]
+  simp only [← ConcreteCategory.forget_map_eq_coe, ← Functor.comp_map, ← α.homEquiv_comp]
   rw [_root_.mul_comm]
   simp
-  rfl
 
 end CommGrp_
