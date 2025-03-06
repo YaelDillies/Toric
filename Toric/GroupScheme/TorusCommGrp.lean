71247736e254f5eda5d9556ea9bd84779ce9f43d
Kiolt <38689041+Kiolt@users.noreply.github.com>
2025-03-06 18:08:15 +0000

    Tori are abelian group schemes!

---
+++ Toric/GroupScheme/TorusCommGrp.lean
@@ -0,0 +1,16 @@
+import Toric.GroupScheme.GroupScheme
+import Mathlib.CategoryTheory.Monoidal.Grp_
+import Toric.Mathlib.CategoryTheory.Monoidal.Grp_
+import Toric.Mathlib.CategoryTheory.Monoidal.CommMon_
+
+open CategoryTheory AlgebraicGeometry Opposite
+
+attribute [local instance] ChosenFiniteProducts.ofFiniteProducts
+
+noncomputable instance (σ : Type*) : Grp_Class (AlgebraicGeometry.TorusInt σ) :=
+  Grp_Class.ofRepresentableBy _ (Scheme.Γ ⋙ forget₂ _ CommMonCat ⋙ CommMonCat.units ⋙
+      CommGrp.coyonedaRight.obj (op σ) ⋙ forget₂ _ Grp) (TorusInt.representableBy σ)
+
+instance (σ : Type*) : IsCommMon (TorusInt σ) :=
+  IsCommMon.ofRepresentableBy _ (Scheme.Γ ⋙ forget₂ _ CommMonCat ⋙ CommMonCat.units ⋙
+      CommGrp.coyonedaRight.obj (op σ) ⋙ forget₂ _ CommMonCat) (TorusInt.representableBy σ)
