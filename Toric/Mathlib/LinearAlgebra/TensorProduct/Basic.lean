347a92924a34fc14166a3da5a85e96df09fa36b8
Yaël Dillies <yael.dillies@gmail.com>
2025-03-09 11:39:09 +0000

    Prune useless variables

--- Toric/Mathlib/LinearAlgebra/TensorProduct/Basic.lean
+++ Toric/Mathlib/LinearAlgebra/TensorProduct/Basic.lean
@@ -1,15 +1,8 @@
 import Mathlib.LinearAlgebra.LinearIndependent.Defs
 import Mathlib.LinearAlgebra.TensorProduct.Basic
 
-variable {R : Type*} [CommSemiring R]
-variable {R' : Type*} [Monoid R']
-variable {R'' : Type*} [Semiring R'']
-variable {A M N P Q S T : Type*}
-variable [AddCommMonoid M] [AddCommMonoid N] [AddCommMonoid P]
-variable [AddCommMonoid Q] [AddCommMonoid S] [AddCommMonoid T]
-variable [Module R M] [Module R N] [Module R Q] [Module R S] [Module R T]
-variable [DistribMulAction R' M]
-variable [Module R'' M]
+variable {R M N : Type*} [CommSemiring R] [AddCommMonoid M] [AddCommMonoid N]
+  [Module R M] [Module R N]
 
 protected lemma LinearIndependent.tmul {ι κ : Type*} {f : ι → M} {g : κ → N}
     (hf : LinearIndependent R f) (hg : LinearIndependent R g) :
