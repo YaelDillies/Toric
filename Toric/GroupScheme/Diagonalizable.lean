1fb0c077c3b6883fe249d52d4756eff84b4020af
Yaël Dillies <yael.dillies@gmail.com>
2025-03-15 21:00:56 +0000

    State the group-like condition for diagonalisability

--- Toric/GroupScheme/Diagonalizable.lean
+++ Toric/GroupScheme/Diagonalizable.lean
@@ -4,9 +4,11 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies, Michał Mrugała
 -/
 import Mathlib.CategoryTheory.Monoidal.Grp_
+import Mathlib.RingTheory.HopfAlgebra.MonoidAlgebra
 import Toric.GroupScheme.SchemeOver
+import Toric.Hopf.GroupLike
 
-open CategoryTheory AlgebraicGeometry
+open AlgebraicGeometry CategoryTheory Coalgebra Opposite
 
 universe u
 
@@ -21,10 +23,12 @@ instance :
 end
 
 namespace AlgebraicGeometry.Scheme
-variable  {R : CommRingCat.{u}} {G : Over (Spec R)} [Grp_Class G]
+section CommRing
+variable {R : CommRingCat.{u}} {G : Over (Spec R)} [Grp_Class G]
     {A : Type u} [CommGroup A] [Monoid.FG A]
 
 variable (G) in
+@[mk_iff]
 class IsDiagonalisable : Prop where
   existsIso :
     ∃ (A : Type u) (_ : CommGroup A) (_ : Monoid.FG A),
@@ -35,4 +39,21 @@ instance :
     IsDiagonalisable <| .mk <| Spec.map <| CommRingCat.ofHom <| algebraMap R <| MonoidAlgebra R A :=
   ⟨⟨A, _, ‹_›, sorry⟩⟩
 
+end CommRing
+
+section Field
+variable {K : Type*} [Field K] {G : Over (Spec <| .of K)} [Grp_Class G]
+
+instance : HopfAlgebra K (Γ.obj <| op G.left) := sorry
+
+/-- An algebraic group `G` over a field `K` is diagonalisable iff `Γ(G)` is `K`-spanned by its
+group-like elements.
+
+Note that this is more generally true over arbitrary commutative rings, but we do not prove that.
+See SGA III, Exposé VIII for more info. -/
+lemma isDiagonalisable_iff_span_isGroupLikeElem_eq_top :
+    IsDiagonalisable G ↔
+      Submodule.span K {a : Γ.obj <| op G.left | IsGroupLikeElem K a} = ⊤ := sorry
+
+end Field
 end AlgebraicGeometry.Scheme
