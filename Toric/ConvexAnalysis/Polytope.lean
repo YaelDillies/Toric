f254184646bffa20e31596415513191397a6dc77
mj141592 <mjohnson31415926@gmail.com>
2025-03-15 23:12:58 +0000

    Add polytope add, neg and sub

--- Toric/ConvexAnalysis/Polytope.lean
+++ Toric/ConvexAnalysis/Polytope.lean
@@ -4,7 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Matthew Johnson, Yaël Dillies
 -/
 import Mathlib.Analysis.Convex.Hull
-
+import Mathlib.Analysis.Convex.Combination
+import Mathlib.Algebra.Group.Pointwise.Finset.Basic
 /-!
 # Polytopes
 
@@ -13,9 +14,8 @@ We define polytopes as convex hulls of finite sets.
 
 open scoped Pointwise
 
-variable {R E : Type*} [OrderedSemiring R]
-
 section AddCommMonoid
+variable {R E : Type*} [OrderedSemiring R]
 variable [AddCommMonoid E] [Module R E] {s t : Set E} {x y : E}
 
 variable (R) in
@@ -31,17 +31,44 @@ def IsPolytope (s : Set E) : Prop := ∃ t : Finset E, s = convexHull R t
 @[simp]
 lemma IsPolytope.convexHull_Finset {s : Finset E} : IsPolytope R <| convexHull R s.toSet := by use s
 
-protected lemma IsPolytope.add (hs : IsPolytope R s) (ht : IsPolytope R t) :
-    IsPolytope R (s + t) := by sorry
-
 end AddCommMonoid
 
 section AddCommGroup
+variable {R E : Type*} [LinearOrderedField R]
 variable [AddCommGroup E] [Module R E] {s t : Set E} {x y : E}
 
-protected lemma IsPolytope.neg (hs : IsPolytope R s) : IsPolytope R (-s) := by sorry
+protected lemma IsPolytope.add (hs : IsPolytope R s) (ht : IsPolytope R t) :
+    IsPolytope R (s + t) := by
+    unfold IsPolytope
+    rcases hs with ⟨A, hA⟩
+    rcases ht with ⟨B, hB⟩
+    classical
+    rw [hB, hA]
+    use ((↑A) + (↑B))
+    simp
+    rw [convexHull_add A.toSet B.toSet]
+
+
+protected lemma IsPolytope.neg (hs : IsPolytope R s) :
+    IsPolytope R (-s) := by
+    unfold IsPolytope
+    rcases hs with ⟨A, hA⟩
+    classical
+    rw [hA]
+    use -A
+    rw [← convexHull_neg A.toSet]
+    simp
+
 
 protected lemma IsPolytope.sub (hs : IsPolytope R s) (ht : IsPolytope R t) :
-    IsPolytope R (s - t) := by sorry
+    IsPolytope R (s - t) := by
+    unfold IsPolytope
+    rcases hs with ⟨A, hA⟩
+    rcases ht with ⟨B, hB⟩
+    rw [hA, hB]
+    classical
+    use ((↑A) - (↑B))
+    simp
+    rw [convexHull_sub A.toSet B.toSet]
 
 end AddCommGroup
