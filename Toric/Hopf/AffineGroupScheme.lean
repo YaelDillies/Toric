bf61bdfb5d7cba5134bf6e4967d2d562dd182b59
Yaël Dillies <yael.dillies@gmail.com>
2025-03-20 16:59:37 +0000

    progress

--- Toric/Hopf/AffineGroupScheme.lean
+++ Toric/Hopf/AffineGroupScheme.lean
@@ -4,14 +4,11 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies, Christian Merten, Michał Mrugała, Andrew Yang
 -/
 import Mathlib.AlgebraicGeometry.Pullbacks
+import Mathlib.RingTheory.HopfAlgebra.Basic
 import Toric.Mathlib.Algebra.Category.Ring.Under.Basic
 import Toric.Mathlib.AlgebraicGeometry.AffineScheme
-import Mathlib.RingTheory.HopfAlgebra.Basic
-import Mathlib.Algebra.Category.Ring.Under.Basic
-import Toric.Mathlib.CategoryTheory.ChosenFiniteProducts.Over
 import Toric.Mathlib.CategoryTheory.Limits.Preserves.Finite
 import Toric.Mathlib.CategoryTheory.Monoidal.Grp_
-import Toric.Mathlib.RingTheory.HopfAlgebra.Basic
 
 /-!
 # The equivalence between Hopf algebras and affine group schemes
@@ -74,16 +71,13 @@ object under `R`, and vice versa.
 section Michal
 variable {R : CommRingCat} {A : Type*} [CommRing A] [HopfAlgebra R A]
 
-open MonoidalCategory in
-example : (op (R.mkUnder A)) ⊗ (op (R.mkUnder A)) = op (R.mkUnder (TensorProduct R A A)) := rfl
+open CommRingCat
 
 noncomputable instance : Grp_Class <| op <| CommRingCat.mkUnder R A where
   one := op (Bialgebra.counitAlgHom R A).toUnder
   mul := op <| by
-    refine (Bialgebra.comulAlgHom R A).toUnder ≫ ?_
-    change _ ⟶ Under.mk _
-    simp [pushout.inr, WalkingSpan.right, WalkingPair.right, span]
-    sorry -- (Bialgebra.comulAlgHom R A).toUnder
+    refine (Bialgebra.comulAlgHom R A).toUnder ≫ (Algebra.TensorProduct.map ?_ ?_).toUnder <;>
+      exact @AlgHom.mk _ _ _ _ _ _ _ (_) (.id _) fun _ ↦ rfl
   one_mul' := sorry
   mul_one' := sorry
   mul_assoc' := sorry
