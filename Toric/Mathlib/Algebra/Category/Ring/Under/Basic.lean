b2d4dfb71c341b17d2597e36bf46fb79ab01eec7
Yaël Dillies <yael.dillies@gmail.com>
2025-03-20 16:59:37 +0000

    progress

---
+++ Toric/Mathlib/Algebra/Category/Ring/Under/Basic.lean
@@ -0,0 +1,24 @@
+import Mathlib.Algebra.Category.Ring.Under.Basic
+import Mathlib.CategoryTheory.ChosenFiniteProducts
+import Toric.Mathlib.CategoryTheory.Limits.Shapes.BinaryProducts
+import Toric.Mathlib.CategoryTheory.Limits.Shapes.Pullback.PullbackCone
+
+open CategoryTheory Limits Opposite
+
+namespace CommRingCat.Under
+variable {R : CommRingCat}
+
+/-- A choice of finite products of `(Under R)ᵒᵖ` for `R : CommRingCat` given by the tensor product.
+-/
+noncomputable instance chosenFiniteProducts : ChosenFiniteProducts (Under R)ᵒᵖ where
+  product X Y := {
+    cone := BinaryCofan.op <| pushoutCocone.toBinaryCofan.obj <|
+      pushoutCocone R (unop X).right (unop Y).right
+    isLimit := BinaryCofan.IsColimit.op <| pushoutCocone.IsColimit.toBinaryCofanIsColimit <|
+        CommRingCat.pushoutCoconeIsColimit R _ _
+  }
+  terminal := ⟨_, terminalOpOfInitial Under.mkIdInitial⟩
+
+instance : PreservesFiniteProducts (Under.opToOverOp R) := sorry
+
+end CommRingCat.Under
