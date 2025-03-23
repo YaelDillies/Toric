805b0a58c35b594ff6330e06a6a01efabe24c24b
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2025-03-23 21:16:05 +0100

    apply suggestions from code review

--- Toric/ConvexAnalysis/PolyhedralCone.lean
+++ Toric/ConvexAnalysis/PolyhedralCone.lean
@@ -13,35 +13,30 @@ We define the pointed cone hull and what it means for a pointed cone to be polyh
 
 variable {𝕜 E : Type*} [OrderedSemiring 𝕜] [AddCommMonoid E] [Module 𝕜 E]
 
+namespace PointedCone
+
 variable (𝕜) in
 /-- The span of a set `S` is the smallest pointed cone that contains `S`.
 Pointed cones being defined as submodules over nonnegative scalars, this is exactly the
 submodule span of `S` w.r.t. nonnegative scalars. -/
-def PointedCone.span (S : Set E) : PointedCone 𝕜 E :=
+abbrev span (S : Set E) : PointedCone 𝕜 E :=
   Submodule.span _ S
 
-theorem PointedCone.subset_span {S : Set E} :
+theorem subset_span {S : Set E} :
     S ⊆ PointedCone.span 𝕜 S :=
   Submodule.subset_span
 
-theorem PointedCone.span_eq_submoduleSpan {S : Set E} :
-    PointedCone.span 𝕜 S = Submodule.span _ S := rfl
-
 /-- A pointed cone is polyhedral if it is the convex hull of finitely many points. -/
-def PointedCone.IsPolyhedral (c : PointedCone 𝕜 E) : Prop :=
+def IsPolyhedral (c : PointedCone 𝕜 E) : Prop :=
   ∃ t : Finset E, c = PointedCone.span 𝕜 t
 
-def PointedCone.isPolyhedral_def {c : PointedCone 𝕜 E} :
-    c.IsPolyhedral ↔ ∃ s : Finset E, c = PointedCone.span 𝕜 s :=
-  Iff.rfl
-
-def PointedCone.isPolyhedral_span (s : Set E) [Fintype s] :
+protected def IsPolyhedral.span (s : Set E) (h : s.Finite) :
     (PointedCone.span 𝕜 s).IsPolyhedral :=
-  ⟨s.toFinset, by simp⟩
+  ⟨h.toFinset, by simp⟩
 
 @[simp]
-theorem PointedCone.IsPolyhedral.bot :
+theorem IsPolyhedral.bot :
     (⊥ : PointedCone 𝕜 E).IsPolyhedral := by
-  refine ⟨{0}, ?_⟩
-  ext
-  simp [span]
+  refine ⟨{0}, by simp⟩
+
+end PointedCone
