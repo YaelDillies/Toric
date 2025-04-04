96bedbc07470dc6821b6548fdb0fcef0948bf223
Yaël Dillies <yael.dillies@gmail.com>
2025-04-04 15:19:39 +0000

    `AddMonoidAlgebra` versions

--- Toric/Hopf/MonoidAlgebra.lean
+++ Toric/Hopf/MonoidAlgebra.lean
@@ -18,12 +18,11 @@ of the image of the inclusion `G → k[G]`.
 
 open Coalgebra
 
-namespace MonoidAlgebra
 variable {K R A G H : Type*}
 
+namespace MonoidAlgebra
 section CommSemiring
-variable [CommSemiring R] [Semiring A] [HopfAlgebra R A] [Group G]
-  {x : MonoidAlgebra A G}
+variable [CommSemiring R] [Semiring A] [HopfAlgebra R A] [Group G] {x : MonoidAlgebra A G}
 
 lemma isGroupLikeElem_of (g : G) : IsGroupLikeElem R (of A G g) where
   isUnit := .map _ <| Group.isUnit _
@@ -36,11 +35,9 @@ lemma isGroupLikeElem_single (g : G) : IsGroupLikeElem R (single g 1 : MonoidAlg
 end CommSemiring
 
 section Field
-
 variable [Field K] [Group G]
 
 section Group
-
 variable [Group H]
 
 open Submodule in
@@ -87,7 +84,7 @@ protected lemma single_mapDomainOfBialgHom (f : MonoidAlgebra K G →ₐc[K] Mon
 
 @[simp]
 lemma mapDomainBialgHom_mapDomainOfBialgHom (f : MonoidAlgebra K G →ₐc[K] MonoidAlgebra K H) :
-    mapDomainBialgHom (mapDomainOfBialgHom f) = f :=
+    mapDomainBialgHom K (mapDomainOfBialgHom f) = f :=
   BialgHom.coe_algHom_injective <| algHom_ext fun x ↦ by
     simpa [-single_mapDomainOfBialgHomFun_one] using single_mapDomainOfBialgHomFun_one f x
 
@@ -99,7 +96,7 @@ lemma mapDomainBialgHom_mapDomainOfBialgHom (f : MonoidAlgebra K G →ₐc[K] Mo
 over a field. -/
 noncomputable def mapDomainBialgHomEquiv :
     (G →* H) ≃ (MonoidAlgebra K G →ₐc[K] MonoidAlgebra K H) where
-  toFun := mapDomainBialgHom
+  toFun := mapDomainBialgHom K
   invFun := mapDomainOfBialgHom
   left_inv := mapDomainOfBialgHom_mapDomainBialgHom
   right_inv := mapDomainBialgHom_mapDomainOfBialgHom
@@ -107,12 +104,12 @@ noncomputable def mapDomainBialgHomEquiv :
 end Group
 
 section CommGroup
-
 variable [CommGroup H]
 
---TODO: Yaël fix diamonds!!!!!!!!!!!!!!!!!!!!!!!!
+-- TODO(Yaël): Fix diamond with multiplication as composition
 noncomputable
-instance : Group <| MonoidAlgebra K G →ₐc[K] MonoidAlgebra K H := mapDomainBialgHomEquiv.symm.group
+instance : CommGroup <| MonoidAlgebra K G →ₐc[K] MonoidAlgebra K H :=
+  mapDomainBialgHomEquiv.symm.commGroup
 
 /-- The group isomorphism between group homs `G → H` and bialgebra homs `K[G] → K[H]` of group
 algebras over a field. -/
@@ -121,7 +118,107 @@ noncomputable def mapDomainBialgHomMulEquiv :
   mapDomainBialgHomEquiv.symm.mulEquiv.symm
 
 end CommGroup
-
 end Field
-
 end MonoidAlgebra
+
+
+namespace AddMonoidAlgebra
+section CommSemiring
+variable [CommSemiring R] [Semiring A] [HopfAlgebra R A] [AddGroup G] {x : A[G]}
+
+lemma isGroupLikeElem_of (g : G) : IsGroupLikeElem R (of A G g) where
+  isUnit := .map _ <| Group.isUnit _
+  comul_eq_tmul_self := by simp; rfl
+
+@[simp]
+lemma isGroupLikeElem_single (g : G) : IsGroupLikeElem R (single g 1 : A[G]) :=
+  isGroupLikeElem_of _
+
+end CommSemiring
+
+section Field
+variable [Field K] [AddGroup G]
+
+section AddGroup
+variable [AddGroup H]
+
+open Submodule in
+@[simp]
+lemma isGroupLikeElem_iff_mem_range_of {x : K[G]} :
+    IsGroupLikeElem K x ↔ x ∈ Set.range (of K G) where
+  mp hx := by
+    by_contra h
+    have : LinearIndepOn K id (insert x <| .range (of K G)) :=
+      linearIndepOn_isGroupLikeElem.mono <| by simp [Set.subset_def, hx]
+    rw [linearIndepOn_insert h, id_eq, Set.image_id, ← x.sum_single] at this
+    refine this.2 <| sum_mem fun g hg ↦ ?_
+    rw [← mul_one (x g), ← smul_eq_mul, ← smul_single]
+    refine smul_mem _ _ <| subset_span <| Set.mem_range_self _
+  mpr := by rintro ⟨g, rfl⟩; exact isGroupLikeElem_of _
+
+private noncomputable def mapDomainOfBialgHomFun (f : K[G] →ₐc[K] K[H]) : G → H :=
+  fun g ↦ (isGroupLikeElem_iff_mem_range_of.1 <| (isGroupLikeElem_of g).map f).choose
+
+@[simp]
+private lemma single_mapDomainOfBialgHomFun_one (f : K[G] →ₐc[K] K[H]) (g : G) :
+    single (mapDomainOfBialgHomFun f g) 1 = f (single g 1) :=
+  (isGroupLikeElem_iff_mem_range_of.1 <| (isGroupLikeElem_of g).map f).choose_spec
+
+open Coalgebra in
+/-- A bialgebra homomorphism `K[G] → K[H]` between group algebras over a field `K` comes from a
+group hom `G → H`. This is that group hom, namely the inverse of
+`AddMonoidAlgebra.mapDomainBialgHom`. -/
+noncomputable
+def mapDomainOfBialgHom (f : K[G] →ₐc[K] K[H]) : G →+ H where
+  toFun := mapDomainOfBialgHomFun f
+  map_zero' := Multiplicative.ofAdd.injective <| of_injective (k := K) <| by simp [← one_def]
+  map_add' g₁ g₂ := by
+    refine Multiplicative.ofAdd.injective <| of_injective (k := K) ?_
+    simp only [of_apply, single_mapDomainOfBialgHomFun_one, toAdd_ofAdd, ofAdd_add, toAdd_mul,
+      single_mapDomainOfBialgHomFun_one]
+    rw [← mul_one (1 : K), ← single_mul_single, ← single_mul_single, map_mul]
+    simp
+
+protected lemma single_mapDomainOfBialgHom (f : K[G] →ₐc[K] K[H])
+    (g : G) (k : K) : single (mapDomainOfBialgHom f g) k = f (single g k) := by
+  rw [← mul_one k, ← smul_eq_mul, ← smul_single, ← smul_single, map_smul]
+  exact congr(k • $(single_mapDomainOfBialgHomFun_one f g))
+
+@[simp]
+lemma mapDomainBialgHom_mapDomainOfBialgHom (f : K[G] →ₐc[K] K[H]) :
+    mapDomainBialgHom K (mapDomainOfBialgHom f) = f :=
+  BialgHom.coe_algHom_injective <| algHom_ext fun x ↦ by
+    simpa [-single_mapDomainOfBialgHomFun_one] using single_mapDomainOfBialgHomFun_one f x
+
+@[simp] lemma mapDomainOfBialgHom_mapDomainBialgHom (f : G →+ H) :
+    mapDomainOfBialgHom (mapDomainBialgHom K f) = f := by
+  ext g
+  refine Multiplicative.ofAdd.injective <| of_injective (k := K) ?_
+  simp [AddMonoidAlgebra.single_mapDomainOfBialgHom]
+
+/-- The equivalence between group homs `G → H` and bialgebra homs `K[G] → K[H]` of group algebras
+over a field. -/
+noncomputable def mapDomainBialgHomEquiv :
+    (G →+ H) ≃ (K[G] →ₐc[K] K[H]) where
+  toFun := mapDomainBialgHom K
+  invFun := mapDomainOfBialgHom
+  left_inv := mapDomainOfBialgHom_mapDomainBialgHom
+  right_inv := mapDomainBialgHom_mapDomainOfBialgHom
+
+end AddGroup
+
+section AddCommGroup
+variable [AddCommGroup H]
+
+-- TODO(Yaël): Fix diamond with multiplication as composition
+noncomputable
+instance : AddCommGroup <| K[G] →ₐc[K] K[H] := mapDomainBialgHomEquiv.symm.addCommGroup
+
+/-- The group isomorphism between group homs `G → H` and bialgebra homs `K[G] → K[H]` of group
+algebras over a field. -/
+noncomputable def mapDomainBialgHomMulEquiv : (G →+ H) ≃+ (K[G] →ₐc[K] K[H]) :=
+  mapDomainBialgHomEquiv.symm.addEquiv.symm
+
+end AddCommGroup
+end Field
+end AddMonoidAlgebra
--- Toric/Mathlib/RingTheory/Bialgebra/MonoidAlgebra.lean
+++ Toric/Mathlib/RingTheory/Bialgebra/MonoidAlgebra.lean
@@ -4,8 +4,10 @@ import Mathlib.RingTheory.Bialgebra.MonoidAlgebra
 open Coalgebra
 
 namespace MonoidAlgebra
-variable {R A M N : Type*} [CommSemiring R] [Monoid M] [Monoid N]
+variable {R M N : Type*} [CommSemiring R] [Monoid M] [Monoid N]
 
+-- TODO: Generalise to `MonoidAlgebra A M →ₐc[R] MonoidAlgebra A N` under `Bialgebra R A`
+variable (R) in
 /-- If `f : M → N` is a monoid hom, then `MonoidAlgebra.mapDomain f` is a bialgebra hom between
 their monoid algebras. -/
 @[simps]
@@ -16,3 +18,19 @@ noncomputable def mapDomainBialgHom (f : M →* N) : MonoidAlgebra R M →ₐc[R
   map_comp_comul := by ext; simp
 
 end MonoidAlgebra
+
+namespace AddMonoidAlgebra
+variable {R A M N : Type*} [CommSemiring R] [AddMonoid M] [AddMonoid N]
+
+-- TODO: Generalise to `A[M] →ₐc[R] A[N]` under `Bialgebra R A`
+variable (R) in
+/-- If `f : M → N` is a monoid hom, then `AddMonoidAlgebra.mapDomain f` is a bialgebra hom between
+their monoid algebras. -/
+@[simps]
+noncomputable def mapDomainBialgHom (f : M →+ N) : R[M] →ₐc[R] R[N] where
+  __ := mapDomainAlgHom R R f
+  map_smul' m x := by simp
+  counit_comp := by ext; simp
+  map_comp_comul := by ext; simp
+
+end AddMonoidAlgebra
