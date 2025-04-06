da0e6e4890b8a51df8dd9dd40a312975c5a9e072
Kiolt <kiolterino@gmail.com>
2025-04-06 22:30:03 +0200

    convolution multiplication ring structure

--- Toric/Hopf/CoalgToAlg.lean
+++ Toric/Hopf/CoalgToAlg.lean
@@ -41,45 +41,60 @@ lemma mul_def (f g : C →ₗ[R] A) : f * g = mul' R A ∘ₗ TensorProduct.map 
 @[simp]
 lemma mul_apply'' (f g : C →ₗ[R] A) (c : C) : (f * g) c = mul' R A (.map f g (comul c)) := rfl
 
-local notation "η" => Algebra.linearMap
-local notation "ε" => counit
+local notation "η" => Algebra.linearMap R A
+local notation "ε" => counit (R := R) (A := C)
 local notation "μ" => mul' R A
 local notation "δ" => comul
 local infix:70 " ⊗ₘ " => TensorProduct.map
 -- local notation "α" => TensorProduct.assoc _ _ _
+
+lemma conv_mul_assoc (f g h : C →ₗ[R] A) : f * g * h = f * (g * h) := calc
+      μ ∘ₗ (μ ∘ₗ (f ⊗ₘ g) ∘ₗ δ ⊗ₘ h) ∘ₗ δ
+  _ = (μ ∘ₗ .rTensor _ μ) ∘ₗ ((f ⊗ₘ g) ⊗ₘ h) ∘ₗ (.rTensor _ δ ∘ₗ δ) := by
+    rw [comp_assoc, ← comp_assoc _ _ (rTensor _ _), rTensor_comp_map,
+      ← comp_assoc _ (rTensor _ _), map_comp_rTensor, comp_assoc]
+  _ = (μ ∘ₗ rTensor _ μ)
+      ∘ₗ (((f ⊗ₘ g) ⊗ₘ h) ∘ₗ (TensorProduct.assoc R C C C).symm) ∘ₗ lTensor C δ ∘ₗ δ := by
+    simp only [comp_assoc, coassoc_symm]
+  _ = (μ ∘ₗ rTensor A μ ∘ₗ ↑(TensorProduct.assoc R A A A).symm)
+      ∘ₗ (f ⊗ₘ (g ⊗ₘ h)) ∘ₗ lTensor C δ ∘ₗ δ := by
+    simp only [map_map_comp_assoc_symm_eq, comp_assoc]
+  _ = (μ ∘ₗ .lTensor _ μ) ∘ₗ (f ⊗ₘ (g ⊗ₘ h)) ∘ₗ (lTensor C δ ∘ₗ δ) := by
+    congr 1
+    ext
+    simp [mul_assoc]
+  _ = μ ∘ₗ (f ⊗ₘ μ ∘ₗ (g ⊗ₘ h) ∘ₗ δ) ∘ₗ δ := by
+    rw [comp_assoc, ← comp_assoc _ _ (lTensor _ _), lTensor_comp_map,
+      ← comp_assoc _ (lTensor _ _), map_comp_lTensor, comp_assoc]
+
+lemma conv_one_mul (f : C →ₗ[R] A) : 1 * f = f := calc
+      μ ∘ₗ ((η ∘ₗ ε) ⊗ₘ f) ∘ₗ δ
+  _ = μ ∘ₗ ((η ⊗ₘ f) ∘ₗ rTensor C ε) ∘ₗ δ  := by simp only [map_comp_rTensor]
+  _ = μ ∘ₗ (η ⊗ₘ f) ∘ₗ (rTensor C ε) ∘ₗ δ := by rw [comp_assoc]
+  _ = μ ∘ₗ (rTensor A η ∘ₗ lTensor R f) ∘ₗ rTensor C ε ∘ₗ δ := by simp
+  _ = (μ ∘ₗ rTensor A η) ∘ₗ lTensor R f ∘ₗ (rTensor C ε ∘ₗ δ) := by simp only [comp_assoc]
+  _ = (μ ∘ₗ rTensor A η) ∘ₗ lTensor R f ∘ₗ ((TensorProduct.mk R R C) 1) := by
+    rw [rTensor_counit_comp_comul]
+  _ = f := by ext; simp
+
+lemma conv_mul_one (f : C →ₗ[R] A) : f * 1 = f := calc
+      μ ∘ₗ (f ⊗ₘ (η ∘ₗ ε) ) ∘ₗ δ
+  _ = μ ∘ₗ ((f ⊗ₘ η) ∘ₗ lTensor C ε) ∘ₗ δ  := by simp only [map_comp_lTensor]
+  _ = μ ∘ₗ (f ⊗ₘ η) ∘ₗ (lTensor C ε) ∘ₗ δ := by rw [comp_assoc]
+  _ = μ ∘ₗ (lTensor A η ∘ₗ rTensor R f) ∘ₗ lTensor C ε ∘ₗ δ := by simp
+  _ = (μ ∘ₗ lTensor A η) ∘ₗ rTensor R f ∘ₗ (lTensor C ε ∘ₗ δ) := by simp only [comp_assoc]
+  _ = (μ ∘ₗ lTensor A η) ∘ₗ rTensor R f ∘ₗ ((TensorProduct.mk R C R).flip 1) := by
+    rw [lTensor_counit_comp_comul]
+  _ = f := by ext; simp
 
 instance : Ring (C →ₗ[R] A) where
   left_distrib f g h := by ext; simp [TensorProduct.map_add_right]
   right_distrib f g h := by ext; simp [TensorProduct.map_add_left]
   zero_mul f := by ext; simp
   mul_zero f := by ext; simp
-  mul_assoc f g h := by
-    calc
-          μ ∘ₗ (μ ∘ₗ (f ⊗ₘ g) ∘ₗ δ ⊗ₘ h) ∘ₗ δ
-      _ = (μ ∘ₗ .rTensor _ μ) ∘ₗ ((f ⊗ₘ g) ⊗ₘ h) ∘ₗ (.rTensor _ δ ∘ₗ δ) := by
-        rw [comp_assoc, ← comp_assoc _ _ (rTensor _ _), rTensor_comp_map,
-          ← comp_assoc _ (rTensor _ _), map_comp_rTensor, comp_assoc]
-      _ = (μ ∘ₗ .lTensor _ μ) ∘ₗ (f ⊗ₘ (g ⊗ₘ h)) ∘ₗ (lTensor C δ ∘ₗ δ) := by
-        rw [← coassoc_symm]
-        -- What's the `Algebra` equivalent of `coassoc`?
-        -- Probably associativity of multiplication :)
-        sorry
-      _ = μ ∘ₗ (f ⊗ₘ μ ∘ₗ (g ⊗ₘ h) ∘ₗ δ) ∘ₗ δ := by
-        rw [comp_assoc, ← comp_assoc _ _ (lTensor _ _), lTensor_comp_map,
-          ← comp_assoc _ (lTensor _ _), map_comp_lTensor, comp_assoc]
-  one_mul f := by
-    stop
-    calc
-          μ ∘ₗ (η ∘ₗ ε ⊗ₘ f) ∘ₗ δ
-      _ = (μ ∘ₗ rTensor _ η) ∘ₗ lTensor _ f ∘ₗ (lTensor _ ε ∘ₗ δ) := sorry
-      _ = _ := _
-      _ = f := sorry
-  mul_one f := by
-    stop
-    calc
-          μ ∘ₗ (f ⊗ₘ η ∘ₗ ε) ∘ₗ δ
-      _ = (μ ∘ₗ lTensor _ η) ∘ₗ rTensor _ f ∘ₗ (rTensor _ ε ∘ₗ δ) := sorry
-      _ = f := sorry
+  mul_assoc := conv_mul_assoc
+  one_mul := conv_one_mul
+  mul_one := conv_mul_one
 
 end LinearMap
 
