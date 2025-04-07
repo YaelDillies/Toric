d31e7b3fe4442998e1aeb3a6035cf9ff8bcd2442
Paul-Lez <paul.lezeau@gmail.com>
2025-04-07 09:39:27 +0100

    Remove commented out definitions

--- Toric/ToricVariety/Defs.lean
+++ Toric/ToricVariety/Defs.lean
@@ -28,37 +28,6 @@ attribute [local instance] ChosenFiniteProducts.ofFiniteProducts
 
 open Action_Class
 
---TODO: add group action axioms
---TODO: make a general definition of a group object action
-variable (R n)
--- /-- A toric variety of dimension `n` over a ring `R` is a scheme `X` equipped with a dense
---embedding
--- `Tⁿ → X` and an action `T × X → X` extending the standard action `T × T → T`. -/
--- class ToricVariety (X : Scheme) extends X.Over (Spec R) where
---   /-- The torus embedding -/
---   torusEmb : Torus R n ⟶ X
---   /-- The torus embedding is a morphism over `Spec R`. -/
---   torusEmb_comp_overHom : HomIsOver torusEmb (Spec R) := by aesop_cat
---   /-- The torus embedding is an open immersion. -/
---   [isOpenImmersion_torusEmb : IsOpenImmersion torusEmb]
---   /-- The torus embedding is dominant. -/
---   [isDominant_torusEmb : IsDominant torusEmb]
---   [torusAct : Action_Class (torusOver R n) (X.asOver <| Spec R)]
---   /- The torus action on a toric variety. -/
---   --torusAct : pullback (Torus R n ↘ Spec R) (X ↘ Spec R) ⟶ X
---   /-- The torus action extends the torus multiplication morphism. -/
---   torusMul_comp_torusEmb : (𝟙 (torusOver R n)) ⊗ (Scheme.Hom.asOver torusEmb (Spec R)) ≫
---       (γ : (torusOver R n) ⊗ (X.asOver <| Spec R) ⟶ (X.asOver <| Spec R)) =
---         μ[(torusOver R n)] ≫ (Scheme.Hom.asOver torusEmb (Spec R))
---     -- torusMul ≫
---     -- torusEmb = pullback.lift (pullback.fst _ _) (pullback.snd _ _ ≫ torusEmb)
---     --             (by simp [pullback.condition, torusEmb_comp_overHom]) ≫
---     -- torusAct := by aesop_cat
---   /-There is a monoid action of the torus on `X` -/
---  --torusAction : Action_Class (torusOver R n) (X.asOver <| Spec R)
---   /-- The torus action is compatible with the torus embedding -/
---   t : Bool
-
 /-- A toric variety of dimension `n` over a ring `R` is a scheme `X` equipped with a dense embedding
 `Tⁿ → X` and an action `T × X → X` extending the standard action `T × T → T`. -/
 class ToricVariety (X : Over <| Spec R) where
