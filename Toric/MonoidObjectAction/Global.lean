d8f146d17848a8de9f609fc31a90e7f398722480
Yaël Dillies <yael.dillies@gmail.com>
2025-04-08 15:34:00 +0000

    Delete empty file

--- Toric/MonoidObjectAction/Global.lean
+++
@@ -1,6 +0,0 @@
-import Toric.Mathlib.CategoryTheory.Monoidal.Mon_
-import Mathlib.Algebra.Category.Grp.Adjunctions
-import Mathlib.Algebra.Category.Ring.Adjunctions
-import Mathlib.AlgebraicGeometry.Limits
-import Mathlib.CategoryTheory.Adjunction.Opposites
-import Mathlib.CategoryTheory.Monoidal.Yoneda
