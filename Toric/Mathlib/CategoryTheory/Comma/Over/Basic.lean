498e99d5b91256e278b4f98c755af51952c15424
Yaël Dillies <yael.dillies@gmail.com>
2025-04-08 15:54:15 +0000

    Essential image of a sliced functor

--- Toric/Mathlib/CategoryTheory/Comma/Over/Basic.lean
+++ Toric/Mathlib/CategoryTheory/Comma/Over/Basic.lean
@@ -13,3 +13,22 @@ def equivalenceOfIsTerminal {C : Type*} [Category C] {X : C} (hX : IsTerminal X)
   counitIso := NatIso.ofComponents (fun _ ↦ .refl _)
 
 end CategoryTheory.Over
+
+namespace CategoryTheory.Functor
+variable {C D : Type*} [Category C] [Category D] {X : C} {F : C ⥤ D}
+
+/-- The essential of the slice of a full functor is the same as the essential image of the original
+functor. -/
+@[simp] lemma essImage_overPost [F.Full] {Y : Over (F.obj X)} :
+    (Over.post F (X := X)).essImage Y ↔ F.essImage Y.left where
+  mp := fun ⟨Z, ⟨e⟩⟩ ↦ ⟨Z.left, ⟨(Over.forget _).mapIso e⟩⟩
+  mpr := fun ⟨Z, ⟨e⟩⟩ ↦ let ⟨f, hf⟩ := F.map_surjective (e.hom ≫ Y.hom); ⟨.mk f, ⟨Over.isoMk e⟩⟩
+
+/-- The essential of the slice of a full functor is the same as the essential image of the original
+functor. -/
+@[simp] lemma essImage_underPost [F.Full] {Y : Under (F.obj X)} :
+    (Under.post F (X := X)).essImage Y ↔ F.essImage Y.right where
+  mp := fun ⟨Z, ⟨e⟩⟩ ↦ ⟨Z.right, ⟨(Under.forget _).mapIso e⟩⟩
+  mpr := fun ⟨Z, ⟨e⟩⟩ ↦ let ⟨f, hf⟩ := F.map_surjective (Y.hom ≫ e.inv); ⟨.mk f, ⟨Under.isoMk e⟩⟩
+
+end CategoryTheory.Functor
