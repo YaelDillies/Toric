ffe043ac572a7892db28c87a2e3be5293094bf75
smorel394 <67864981+smorel394@users.noreply.github.com>
2025-04-17 18:36:41 +0200

    Update MonoidAlgebra.lean

--- Toric/Hopf/MonoidAlgebra.lean
+++ Toric/Hopf/MonoidAlgebra.lean
@@ -33,7 +33,7 @@ lemma isGroupLikeElem_single_one (g : G) : IsGroupLikeElem R (single g 1 : Monoi
   isGroupLikeElem_of _
 
 /--
-The group-like elements in a monoid algebra span the whole monoid algebra.
+The group-like elements in a group algebra span the whole group algebra.
 -/
 @[simp]
 lemma span_isGroupLikeElem : Submodule.span A {a : MonoidAlgebra A G | IsGroupLikeElem R a} = ⊤ :=
@@ -41,7 +41,7 @@ lemma span_isGroupLikeElem : Submodule.span A {a : MonoidAlgebra A G | IsGroupLi
     simp [← Finsupp.range_linearCombination]
 
 /--
-A bialgebra isomorphic to a monoid algebra is spanned by its group-like elements.
+A bialgebra isomorphic to a group algebra is spanned by its group-like elements.
 -/
 lemma span_isGroupLikeElem_of_iso {F H : Type*} [Semiring H] [Bialgebra R H]
     [EquivLike F (MonoidAlgebra R G) H] [BialgEquivClass F R (MonoidAlgebra R G) H] (f : F) :
@@ -262,10 +262,10 @@ variable (R A : Type*)
 variable [CommSemiring R] [Semiring A] [Bialgebra R A]
 
 /--
-The `R`-algebra map from the monoid algebra on the group-like elements of `A` to `A`.
+The `R`-algebra map from the group algebra on the group-like elements of `A` to `A`.
 -/
 @[simps!]
-noncomputable def lift_groupLike_algHom :
+noncomputable def liftGroupLikeAlgHom :
     MonoidAlgebra R (GroupLike R A) →ₐ[R] A :=
   MonoidAlgebra.lift R (GroupLike R A) A
   {
@@ -275,15 +275,15 @@ noncomputable def lift_groupLike_algHom :
   }
 
 /--
-The `R`-bialgebra map from the monoid algebra on the group-like elements of `A` to `A`.
+The `R`-bialgebra map from the group algebra on the group-like elements of `A` to `A`.
 -/
 @[simps!]
-noncomputable def lift_groupLike_bialgHom (hinj : Function.Injective (algebraMap R A)) :
+noncomputable def liftGroupLikeBialgHom (hinj : Function.Injective (algebraMap R A)) :
     MonoidAlgebra R (GroupLike R A) →ₐc[R] A :=
   {
-    lift_groupLike_algHom R A with
+    liftGroupLikeAlgHom R A with
     map_smul' := fun a x ↦ by
-      change (lift_groupLike_algHom R A) (a • x) = _
+      change (liftGroupLikeAlgHom R A) (a • x) = _
       simp
     counit_comp := by
       ext x
@@ -295,7 +295,7 @@ noncomputable def lift_groupLike_bialgHom (hinj : Function.Injective (algebraMap
       ext x
       dsimp
       simp only [comul_single, CommSemiring.comul_apply, TensorProduct.map_tmul, lsingle_apply,
-        LinearMap.coe_mk, AddHom.coe_mk, lift_groupLike_algHom_apply, liftNC_single,
+        LinearMap.coe_mk, AddHom.coe_mk, liftGroupLikeAlgHom_apply, liftNC_single,
         AddMonoidHom.coe_coe, map_one, one_mul]
       exact x.2.comul_eq_tmul_self.symm
   }
