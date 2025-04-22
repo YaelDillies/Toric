01b5d4b8c23b94d5225151a0eb3e3761816ce5d6
Yaël Dillies <yael.dillies@gmail.com>
2025-04-22 18:58:32 +0000

    Shorten the name of the commutative group algebra functor

--- Toric/GroupScheme/SpecGrpAlg.lean
+++ Toric/GroupScheme/SpecGrpAlg.lean
@@ -20,8 +20,8 @@ section CommRingCat
 variable (R : CommRingCat)
 
 /-- The diagonalizable group scheme functor. -/
-noncomputable abbrev specCommGrpAlgebra : CommGrpᵒᵖ ⥤ Grp_ (Over (Spec R)) :=
-  commGrpAlgebra R ⋙ hopfSpec R
+noncomputable abbrev specCommGrpAlg : CommGrpᵒᵖ ⥤ Grp_ (Over (Spec R)) :=
+  commGrpAlg R ⋙ hopfSpec R
 
 end CommRingCat
 
@@ -30,11 +30,11 @@ variable {k : Type*} [Field k]
 
 /-- The diagonalizable group scheme functor over a field is fully faithful. -/
 noncomputable
-def specCommGrpAlgebra.fullyFaithful : (specCommGrpAlgebra (.of k)).FullyFaithful :=
-  commGrpAlgebra.fullyFaithful.comp hopfSpec.fullyFaithful
+def specCommGrpAlg.fullyFaithful : (specCommGrpAlg (.of k)).FullyFaithful :=
+  commGrpAlg.fullyFaithful.comp hopfSpec.fullyFaithful
 
-instance specCommGrpAlgebra.instFull : (specCommGrpAlgebra <| .of k).Full := fullyFaithful.full
-instance specCommGrpAlgebra.instFaithful : (specCommGrpAlgebra <| .of k).Faithful :=
+instance specCommGrpAlg.instFull : (specCommGrpAlg <| .of k).Full := fullyFaithful.full
+instance specCommGrpAlg.instFaithful : (specCommGrpAlg <| .of k).Faithful :=
   fullyFaithful.faithful
 
 end Field
--- Toric/Hopf/GrpAlg.lean
+++ Toric/Hopf/GrpAlg.lean
@@ -22,7 +22,7 @@ section CommRing
 variable (R : Type*) [CommRing R]
 
 /-- The functor of commutative group algebras. -/
-noncomputable def commGrpAlgebra : CommGrpᵒᵖ ⥤ Grp_ (CommAlg R)ᵒᵖ where
+noncomputable def commGrpAlg : CommGrpᵒᵖ ⥤ Grp_ (CommAlg R)ᵒᵖ where
   obj G := .mk' <| op <| CommAlg.of R <| MonoidAlgebra R G.unop.carrier
   map f := Grp_.homMk (CommAlg.ofHom <| MonoidAlgebra.mapDomainBialgHom R f.unop.hom).op
   map_comp f g := by ext; simp
@@ -33,14 +33,14 @@ section Field
 variable {k : Type*} [Field k]
 
 /-- The group algebra functor over a field is fully faithful. -/
-noncomputable def commGrpAlgebra.fullyFaithful : (commGrpAlgebra k).FullyFaithful where
+noncomputable def commGrpAlg.fullyFaithful : (commGrpAlg k).FullyFaithful where
   preimage {X Y} f :=
     .op <| CommGrp.ofHom <| MonoidAlgebra.mapDomainOfBialgHom (K := k) <|
       IsMon_Hom.toBialgHom (R := k) f.hom
-  map_preimage {X Y} f := by simp [commGrpAlgebra]; rfl
-  preimage_map {X Y} f := by simp [commGrpAlgebra]
+  map_preimage {X Y} f := by simp [commGrpAlg]; rfl
+  preimage_map {X Y} f := by simp [commGrpAlg]
 
-instance commGrpAlgebra.instFull : (commGrpAlgebra k).Full := fullyFaithful.full
-instance commGrpAlgebra.instFaithful : (commGrpAlgebra k).Faithful := fullyFaithful.faithful
+instance commGrpAlg.instFull : (commGrpAlg k).Full := fullyFaithful.full
+instance commGrpAlg.instFaithful : (commGrpAlg k).Faithful := fullyFaithful.faithful
 
 end Field
