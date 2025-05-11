d1696fe6bdd99e0328058044134a85cf94ea12e9
Yaël Dillies <yael.dillies@gmail.com>
2025-05-11 15:43:02 +0000

    Deduplicate `BialgEquiv.ofBialgHom` and `BialgEquiv.ofBijective`

--- Toric/Mathlib/RingTheory/Bialgebra/Equiv.lean
+++ Toric/Mathlib/RingTheory/Bialgebra/Equiv.lean
@@ -15,19 +15,6 @@ variable {R A B : Type*} [CommSemiring R] [Semiring A] [Semiring B] [Algebra R A
   counit_comp := counit_comp
   map_comp_comul := map_comp_comul
 
-/-- The bialgebra equivalence defined by a bijective bialgebra hom. -/
-@[simps!]
-noncomputable def ofBialgHom (f :  A →ₐc[R] B) (hf : Bijective f) :  A ≃ₐc[R] B where
-  toFun a := f a
-  map_add' := f.map_add'
-  map_smul' := f.map_smul'
-  counit_comp := f.counit_comp
-  map_comp_comul := f.map_comp_comul
-  invFun := Function.surjInv hf.surjective
-  left_inv := Function.leftInverse_surjInv hf
-  right_inv := Function.rightInverse_surjInv _
-  map_mul' := f.map_mul'
-
 /-- Construct a coalgebra equivalence from a bijective coalgebra homomorphism. -/
 noncomputable def ofBijective (f : A →ₐc[R] B) (hf : Function.Bijective f) : A ≃ₐc[R] B :=
   { AlgEquiv.ofBijective (f : A →ₐ[R] B) hf, f with }
