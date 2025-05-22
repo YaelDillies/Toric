ab2461bec813ef34c925e0ab5a9b7b7abe4447d4
Michał Mrugała <kiolterino@gmail.com>
2025-05-22 21:04:28 +0100

    Tensor products start

---
+++ Toric/Hopf/TensorProduct.lean
@@ -0,0 +1,13 @@
+/-
+Copyright (c) 2025 Michał Mrugała. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Michał Mrugała
+-/
+
+import Mathlib
+
+/-!
+
+# Tensor Product of Hopf Algebras
+
+-/
