4c9712ba94ed08ce407900e3b6d2f295cde61396
Yaël Dillies <yael.dillies@gmail.com>
2025-05-25 08:16:44 +0000

    The antipode preserves the counit Taken from https://leanprover.zulipchat.com/#narrow/channel/116395-maths/topic/Coalgebras.2Fbialgebras.2FHopf.20algebras/near/519478088. Thank you Patrick!

--- Toric/Mathlib/RingTheory/HopfAlgebra/Basic.lean
+++ Toric/Mathlib/RingTheory/HopfAlgebra/Basic.lean
@@ -9,6 +9,8 @@ import Mathlib.RingTheory.HopfAlgebra.Basic
 
 export HopfAlgebraStruct (antipode)
 
+open Coalgebra Bialgebra TensorProduct
+
 namespace HopfAlgebra
 variable {R A : Type*} [CommSemiring R] [Semiring A] [HopfAlgebra R A]
 
@@ -19,5 +21,20 @@ lemma _root_.Coalgebra.Repr.algebraMap_counit_eq_sum_antipode_mul {a : A} (𝓡 
 lemma _root_.Coalgebra.Repr.algebraMap_counit_eq_sum_mul_antipode {a : A} (𝓡 : Coalgebra.Repr R a) :
     algebraMap R A (Coalgebra.counit a) =
       ∑ i ∈ 𝓡.index, 𝓡.left i * antipode (R := R) (𝓡.right i) := by simp [← 𝓡.eq]
+
+lemma sum_counit_smul {a : A} (𝓡 : Coalgebra.Repr R a) :
+    ∑ x ∈ 𝓡.index, counit (R := R) (𝓡.left x) • 𝓡.right x = a := by
+  have := sum_counit_tmul_eq  (R := R) 𝓡
+  apply_fun lift (LinearMap.lsmul R A) at this
+  simp_rw [map_sum] at this
+  convert this
+  simp
+
+lemma antipode_counit (a : A) : counit (R := R) (antipode (R := R) a) = counit (R := R) a := by
+  have := sum_mul_antipode_eq_smul (R := R) (ℛ R a)
+  apply_fun counit (R := R) at this
+  simp_rw [map_smul, counit_one, smul_eq_mul, mul_one, map_sum, counit_mul, ← smul_eq_mul,
+    ← map_smul, ← map_sum, sum_counit_smul (ℛ R a)] at this
+  exact this
 
 end HopfAlgebra
