45c0c73c3223a1dafc190a9824eb120372b55617
Yaël Dillies <yael.dillies@gmail.com>
2025-06-02 18:51:10 +0000

    make it simp

--- Toric/Mathlib/Algebra/FreeAbelianGroup/Finsupp.lean
+++ Toric/Mathlib/Algebra/FreeAbelianGroup/Finsupp.lean
@@ -1,5 +1,5 @@
 import Mathlib.Algebra.FreeAbelianGroup.Finsupp
 
-lemma Finsupp.toFreeAbelianGroup_single {σ : Type*} (x : σ) (n : ℕ) :
+@[simp] lemma Finsupp.toFreeAbelianGroup_single {σ : Type*} (x : σ) (n : ℕ) :
     Finsupp.toFreeAbelianGroup (X := σ) (.single x n) = n • .of x := by
   simp [Finsupp.toFreeAbelianGroup]
