2c3779a7a60761d38457dacbe2dd85d79aaf9692
Andrew Yang <the.erd.one@gmail.com>
2025-06-06 18:43:19 +0100

    stash

--- Toric/Examples/U1.lean
+++ Toric/Examples/U1.lean
@@ -3,11 +3,12 @@ import Toric.GroupScheme.Character
 
 noncomputable section
 
-open scoped Polynomial.Bivariate
+notation3:max R "[X][Y]" => Polynomial (Polynomial R)
+notation3:max "Y" => Polynomial.C (Polynomial.X)
 
 open Polynomial TensorProduct
 
-variable {R : Type*} [CommRing R]
+variable {R S : Type*} [CommRing R] [CommRing S] [Algebra R S]
 
 variable (R) in
 abbrev U1Ring : Type _ := R[X][Y] ⧸ Ideal.span {(X ^ 2 + Y ^ 2 - 1 : R[X][Y])}
@@ -29,9 +30,13 @@ instance : Algebra R (U1Ring R ⊗[R] U1Ring R) :=
 instance : CoalgebraStruct R (U1Ring R) where
   comul := (Ideal.Quotient.liftₐ _
     (aevalAEval (.mk X ⊗ₜ .mk X - .mk Y ⊗ₜ .mk Y) (.mk X ⊗ₜ .mk Y - .mk Y ⊗ₜ .mk X)) (by
+    show Ideal.span _ ≤ RingHom.ker _
+    simp only [Ideal.span_le, Set.singleton_subset_iff]
     sorry
     )).toLinearMap
   counit := (Ideal.Quotient.liftₐ _ (aevalAEval 1 0) (by
+    show Ideal.span _ ≤ RingHom.ker _
+    simp only [Ideal.span_le, Set.singleton_subset_iff]
     sorry
     )).toLinearMap
 
@@ -49,7 +54,40 @@ instance : Bialgebra R (U1Ring R) where
 instance : HopfAlgebra R (U1Ring R) where
   antipode := (Ideal.Quotient.liftₐ _
     (aevalAEval (.mk X) (- .mk Y)) (by
+    show Ideal.span _ ≤ RingHom.ker _
+    simp only [Ideal.span_le, Set.singleton_subset_iff]
     sorry
     )).toLinearMap
   mul_antipode_rTensor_comul := sorry
   mul_antipode_lTensor_comul := sorry
+
+def U1Ring.complexEquiv : AddMonoidAlgebra ℂ (Unit →₀ ℤ) ≃ₐc[ℂ] U1Ring ℂ where
+  __ := ((MonoidAlgebra.liftGroupLikeBialgHom _ _).comp
+    (MonoidAlgebra.mapDomainBialgHom ℂ (M := Multiplicative (Unit →₀ ℤ))
+    (AddMonoidHom.toMultiplicative'' (.comp (zmultiplesHom _
+      (.ofMul ⟨.mkOfMulEqOne (α := U1Ring ℂ)
+      (.mk X + Complex.I • .mk Y) (.mk X - Complex.I • .mk Y) (by sorry), (by sorry)⟩))
+      AddEquiv.finsuppUnique.toAddMonoidHom))))
+  invFun := Ideal.Quotient.liftₐ _ (aevalAEval
+    ((1 / 2 : ℂ) • (.single (.single .unit 1) 1 - .single (.single .unit (-1)) 1))
+    (- (.I / 2 : ℂ) • (.single (.single .unit 1) 1 + .single (.single .unit (-1)) 1))) (by
+    show Ideal.span _ ≤ RingHom.ker _
+    simp only [Ideal.span_le, Set.singleton_subset_iff]
+    sorry)
+  left_inv := sorry
+  right_inv := sorry
+
+instance : Algebra S (S ⊗[R] U1Ring R) :=
+  Algebra.TensorProduct.leftAlgebra (A := S) (B := U1Ring R)
+
+def U1Ring.baseChangeEquiv : S ⊗[R] U1Ring R ≃ₐc[S] U1Ring S := by
+  sorry
+
+open AlgebraicGeometry
+open scoped Hom
+
+local notation "Spec(R)" => (Spec (CommRingCat.of R))
+local notation "SO₂(R)" => (Spec (CommRingCat.of (U1Ring R)))
+
+def foo : (Spec(R).asOver Spec(R) ⟶ SO₂(R).asOver Spec(R)) ≃*
+    Matrix.specialOrthogonalGroup (Fin 2) R := sorry
