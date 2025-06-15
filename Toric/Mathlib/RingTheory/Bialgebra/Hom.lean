e469fb964004af54d246b708f562066b91f0c6e2
Yaël Dillies <yael.dillies@gmail.com>
2025-06-15 09:38:25 +0000

    Assorted prerequisite lemmas for SO₂

--- Toric/Mathlib/RingTheory/Bialgebra/Hom.lean
+++ Toric/Mathlib/RingTheory/Bialgebra/Hom.lean
@@ -15,3 +15,16 @@ end BialgHom
 @[simp]
 lemma BialgHom.toLinearMap_apply {R A B : Type*} [CommSemiring R] [Semiring A] [Bialgebra R A]
     [Semiring B] [Bialgebra R B] (f : A →ₐc[R] B) (x : A) : f.toLinearMap x = f x := rfl
+
+namespace BialgHom
+
+open Bialgebra Coalgebra Algebra.TensorProduct
+
+variable {R A B : Type*} [CommSemiring R] [Semiring A] [Bialgebra R A] [Semiring B] [Bialgebra R B]
+
+@[simps!]
+def ofAlgHom' (f : A →ₐ[R] B) (counit_comp : (counitAlgHom R B).comp f = (counitAlgHom R A))
+    (map_comp_comul : (map f f).comp (comulAlgHom _ _) = (comulAlgHom _ _).comp f) :
+    A →ₐc[R] B := .ofAlgHom f congr(($counit_comp).toLinearMap) congr(($map_comp_comul).toLinearMap)
+
+end BialgHom
