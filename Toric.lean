22279d7c8e0eeea77bca45fb75358cdf8cbc32a3
Yaël Dillies <yael.dillies@gmail.com>
2025-06-24 20:39:35 +0000

    The global sections of an affine monoid scheme are a bialgebra

--- Toric.lean
+++ Toric.lean
@@ -28,8 +28,10 @@ import Toric.Mathlib.Algebra.MonoidAlgebra.Basic
 import Toric.Mathlib.Algebra.MonoidAlgebra.Defs
 import Toric.Mathlib.Algebra.MonoidAlgebra.MapDomain
 import Toric.Mathlib.Algebra.Polynomial.AlgebraMap
+import Toric.Mathlib.AlgebraicGeometry.GammaSpecAdjunction
 import Toric.Mathlib.AlgebraicGeometry.Pullbacks
 import Toric.Mathlib.AlgebraicGeometry.Scheme
+import Toric.Mathlib.CategoryTheory.Comma.Over.Basic
 import Toric.Mathlib.CategoryTheory.Comma.Over.OverClass
 import Toric.Mathlib.CategoryTheory.Functor.FullyFaithful
 import Toric.Mathlib.CategoryTheory.Limits.Preserves.Shapes.Over
