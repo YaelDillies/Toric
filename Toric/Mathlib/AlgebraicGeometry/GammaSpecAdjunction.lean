3f7935d244f64ffd671ab017558327bd1f471a5d
Yaël Dillies <yael.dillies@gmail.com>
2025-07-01 21:04:06 +0000

    typo

--- Toric/Mathlib/AlgebraicGeometry/GammaSpecAdjunction.lean
+++ Toric/Mathlib/AlgebraicGeometry/GammaSpecAdjunction.lean
@@ -5,7 +5,7 @@ variable {R S : CommRingCat}
 
 instance : Scheme.Γ.rightOp.IsLeftAdjoint := ΓSpec.adjunction.isLeftAdjoint
 
-@[simp] lemma Spec.map_preimge_unop (f : Spec R ⟶ Spec S) :
+@[simp] lemma Spec.map_preimage_unop (f : Spec R ⟶ Spec S) :
     Spec.map (Spec.fullyFaithful.preimage f).unop = f := Spec.fullyFaithful.map_preimage _
 
 end AlgebraicGeometry
