444b9dd561b08bd778cb74642b3390ee99549e57
Yaël Dillies <yael.dillies@gmail.com>
2025-07-29 07:15:06 +0000

    Bump mathlib

--- Toric/Mathlib/CategoryTheory/Monoidal/Grp_.lean
+++ Toric/Mathlib/CategoryTheory/Monoidal/Grp_.lean
@@ -7,6 +7,4 @@ variable {C : Type*} [Category C] [CartesianMonoidalCategory C] {G H : Grp_ C}
 
 @[simp] lemma forget₂Mon_obj_X (G : Grp_ C) : ((forget₂Mon_ C).obj G).X = G.X := rfl
 
-instance {f : G ⟶ H} [IsIso f] : IsIso f.hom := inferInstanceAs <| IsIso <| (forget C).map f
-
 end Grp_
