616dd204a07e96ba9fc2389075c0a2d5ff1d284e
Yaël Dillies <yael.dillies@gmail.com>
2025-10-17 18:48:00 +0200

    Remove polyhedral cones They belong in a downstream library, probably Polyhedral from Martin and Olivia.

--- Toric.lean
+++ Toric.lean
@@ -19,7 +19,6 @@ import Toric.Mathlib.AlgebraicGeometry.Pullbacks
 import Toric.Mathlib.CategoryTheory.Monoidal.Cartesian.CommGrp_
 import Toric.Mathlib.CategoryTheory.Monoidal.Cartesian.Grp_
 import Toric.Mathlib.CategoryTheory.Monoidal.Cartesian.Mon_
-import Toric.Mathlib.Geometry.Convex.Cone.Polyhedral
 import Toric.Mathlib.RingTheory.AdjoinRoot
 import Toric.Mathlib.RingTheory.Bialgebra.Convolution
 import Toric.Mathlib.RingTheory.Bialgebra.GroupLike
